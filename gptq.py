95cb23ab6229c61a840a205aab75a03b560d6ca1
Miguel <m.pl.braganca@gmail.com>
2024-05-19 15:53:34 +0100

    first commit

---
+++ gptq.py
@@ -0,0 +1,341 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import math
+import time
+from hqq import quantize as hqq_quantize
+
+def quantize(x, scale, zero, maxq):
+    if maxq < 0:
+        return (x > scale / 2).float() * scale + (x < zero / 2).float() * zero
+    q = torch.clamp(torch.round(x / scale) + zero, 0, maxq)
+    return scale * (q - zero)
+
+class Quantizer(nn.Module):
+
+    def __init__(self, shape=1):
+        super(Quantizer, self).__init__()
+        self.register_buffer('maxq', torch.tensor(0))
+        self.register_buffer('scale', torch.zeros(shape))
+        self.register_buffer('zero', torch.zeros(shape))
+
+    def configure(
+        self,
+        bits, perchannel=False, sym=True,
+        mse=False, norm=2.4, grid=100, maxshrink=.8,
+        trits=False
+    ):
+        self.maxq = torch.tensor(2 ** bits - 1) # maximum value with n bits
+        self.perchannel = perchannel
+        self.sym = sym
+        self.mse = mse # search
+        self.norm = norm # norm for mse
+        self.grid = grid # grid for mse
+        self.maxshrink = maxshrink  # max shrink for mse
+        if trits:
+            self.maxq = torch.tensor(-1)
+
+    def find_params(self, x, weight=False):
+        dev = "cuda"
+        self.maxq = self.maxq.to(dev)
+
+        shape = x.shape
+        if self.perchannel:
+            if weight:
+                x = x.flatten(1)
+            else:
+                if len(shape) == 4:
+                    x = x.permute([1, 0, 2, 3])
+                    x = x.flatten(1)
+                if len(shape) == 3:
+                    x = x.reshape((-1, shape[-1])).t()
+                if len(shape) == 2:
+                    x = x.t()
+        else:
+            x = x.flatten().unsqueeze(0)
+
+        tmp = torch.zeros(x.shape[0], device=dev)
+        xmin = torch.minimum(x.min(1)[0], tmp)
+        xmax = torch.maximum(x.max(1)[0], tmp)
+
+        if self.sym:
+            xmax = torch.maximum(torch.abs(xmin), xmax)
+            tmp = xmin < 0
+            if torch.any(tmp):
+                xmin[tmp] = -xmax[tmp]
+        tmp = (xmin == 0) & (xmax == 0)
+        xmin[tmp] = -1
+        xmax[tmp] = +1
+
+        if self.maxq < 0:
+          self.scale = xmax
+          self.zero = xmin
+        else:
+          self.scale = (xmax - xmin) / self.maxq
+          if self.sym:
+              self.zero = torch.full_like(self.scale, (self.maxq + 1) / 2)
+          else: 
+              self.zero = -xmin / self.scale # torch.round(-xmin / self.scale)
+
+        if not self.perchannel:
+            if weight:
+                tmp = shape[0]
+            else:
+                tmp = shape[1] if len(shape) != 3 else shape[2]
+            self.scale = self.scale.repeat(tmp)
+            self.zero = self.zero.repeat(tmp)
+
+        if weight:
+            shape = [-1] + [1] * (len(shape) - 1)
+            self.scale = self.scale.reshape(shape)
+            self.zero = self.zero.reshape(shape)
+            return
+        if len(shape) == 4:
+            self.scale = self.scale.reshape((1, -1, 1, 1))
+            self.zero = self.zero.reshape((1, -1, 1, 1))
+        if len(shape) == 3:
+            self.scale = self.scale.reshape((1, 1, -1))
+            self.zero = self.zero.reshape((1, 1, -1))
+        if len(shape) == 2:
+            self.scale = self.scale.unsqueeze(0)
+            self.zero = self.zero.unsqueeze(0)
+
+    def quantize(self, x):
+        if self.ready():
+            return quantize(x, self.scale, self.zero, self.maxq)
+        return x
+
+    def enabled(self):
+        return self.maxq > 0
+
+    def ready(self):
+        return torch.all(self.scale != 0)
+
+
+DEBUG = False 
+
+torch.backends.cuda.matmul.allow_tf32 = False
+torch.backends.cudnn.allow_tf32 = False
+
+
+class GPTQ:
+
+    def __init__(self, W):
+        self.W = W
+        self.dev = W.device
+        self.rows = W.shape[0]
+        self.columns = W.shape[1]
+        self.H = torch.zeros((self.columns, self.columns), device=self.dev)
+        self.nsamples = 0
+
+    def add_batch(self, inp, out):
+        if DEBUG:
+            self.inp1 = inp
+            self.out1 = out
+        if len(inp.shape) == 2:
+            inp = inp.unsqueeze(0)
+        tmp = inp.shape[0]
+        if len(inp.shape) == 3:
+            inp = inp.reshape((-1, inp.shape[-1]))
+        inp = inp.t()
+        self.H *= self.nsamples / (self.nsamples + tmp)
+        self.nsamples += tmp
+        inp = math.sqrt(2 / self.nsamples) * inp.float()
+        self.H += inp.matmul(inp.t())
+
+    def fasterquant(
+        self, blocksize=128, percdamp=.01, groupsize=-1, actorder=False, static_groups=False, L=0.1
+    ):
+        
+        W = self.W.clone()
+        W = W.float()
+
+        hqq_quant_config = {
+            'weight_quant_params': {'nbits': 4, 'channel_wise': True, 'group_size': 64, 'optimize': True, 'round_zero': True, 'axis': 1, 'view_as_float': False},
+            'scale_quant_params': None,
+            'zero_quant_params': None}
+
+        Q_hqq, meta_hqq = hqq_quantize(W, **hqq_quant_config)
+
+        print(meta_hqq["zero"])
+
+        #self.H = torch.eye(W.shape[1]).to("cuda") # DEBUG
+        self.H = torch.load("rdmHessian.pt").to("cuda")
+        #self.H = torch.eye(W.shape[1]).to("cuda")
+
+
+        tick = time.time()
+
+        if not self.quantizer.ready():
+            self.quantizer.find_params(W, weight=True)
+
+        H = self.H
+        del self.H
+        dead = torch.diag(H) == 0
+        H[dead, dead] = 1
+        W[:, dead] = 0
+
+        if static_groups:
+            import copy
+            groups = []
+            for i in range(0, self.columns, groupsize):
+                quantizer = copy.deepcopy(self.quantizer)
+                quantizer.find_params(W[:, i:(i + groupsize)], weight=True)
+                groups.append(quantizer)
+
+        if actorder:
+            perm = torch.argsort(torch.diag(H), descending=True)
+            W = W[:, perm]
+            H = H[perm][:, perm]
+            invperm = torch.argsort(perm)
+
+        Losses = torch.zeros_like(W)
+        Q = torch.zeros_like(W)
+
+        damp = percdamp * torch.mean(torch.diag(H))
+        diag = torch.arange(self.columns, device=self.dev)
+        H[diag, diag] += damp
+        H = torch.linalg.cholesky(H)
+        H = torch.cholesky_inverse(H)
+        H = torch.linalg.cholesky(H, upper=True)
+        Hinv = H
+
+        for i1 in range(0, self.columns, blocksize):
+            i2 = min(i1 + blocksize, self.columns)
+            count = i2 - i1
+
+            W1 = W[:, i1:i2].clone()
+            Q1 = torch.zeros_like(W1)
+            Q1_hqq = Q_hqq[:, i1:i2]
+
+            Err1 = torch.zeros_like(W1)
+            Losses1 = torch.zeros_like(W1)
+            Hinv1 = Hinv[i1:i2, i1:i2]
+
+            for i in range(count):
+                w = W1[:, i]
+                q_hqq = Q1_hqq[:, i]
+                d = Hinv1[i, i]
+
+                if groupsize != -1:
+                    if not static_groups:
+                        if (i1 + i) % groupsize == 0:
+                            self.quantizer.find_params(W[:, (i1 + i):(i1 + i + groupsize)], weight=True)
+                    else:
+                        idx = i1 + i
+                        if actorder:
+                            idx = perm[idx]
+                        self.quantizer = groups[idx // groupsize]
+
+                q = quantize(
+                    w.unsqueeze(1), self.quantizer.scale, self.quantizer.zero, self.quantizer.maxq
+                ).flatten()
+                Q1[:, i] = q
+                print("zero", self.quantizer.zero)
+                Losses1[:, i] = (w - q) ** 2 / d ** 2
+
+                err1 = ((w - q_hqq) * L + (w - q) * (1 - L)) / d
+                W1[:, i:] -= err1.unsqueeze(1).matmul(Hinv1[i, i:].unsqueeze(0))
+                Err1[:, i] = err1
+
+            Q[:, i1:i2] = Q1
+            Losses[:, i1:i2] = Losses1 / 2
+
+            W[:, i2:] -= Err1.matmul(Hinv[i1:i2, i2:])
+
+            if DEBUG:
+                self.W[:, :i2] = Q[:, :i2]
+                self.W[:, i2:] = W[:, i2:]
+                print(torch.sum(Losses))
+
+        torch.cuda.synchronize()
+        print('time %.2f' % (time.time() - tick))
+        print('error', torch.sum(Losses).item())
+
+        if actorder:
+            Q = Q[:, invperm]
+
+        self.W = Q.reshape(self.W.shape).to(self.W.dtype)
+
+    def free(self):
+        if DEBUG:
+            self.inp1 = None
+            self.out1 = None
+        self.H = None
+        self.Losses = None
+        self.Trace = None
+        torch.cuda.empty_cache()
+
+    def free(self):
+        self.H = None
+        self.Losses = None
+        self.Trace = None
+        torch.cuda.empty_cache()
+
+def generate_positive_definite_matrix(n):
+    A = torch.randn(n, n)
+    A = torch.mm(A, A.t())
+    A += torch.eye(n) * n
+    return A
+
+if __name__ == "__main__":
+    wbits = 4
+    sym = False
+    mse = False
+    perchannel = True
+
+    blocksize=128
+    percdamp = 0.01
+    groupsize = 64
+    act_order = False
+    static_groups = False
+    W = torch.load("W_0.pt")
+
+    gptq = GPTQ(W)  
+    gptq.quantizer = Quantizer()
+    gptq.quantizer.configure(wbits, perchannel=perchannel, sym=sym, mse=mse)
+    gptq.quantizer.find_params(W, weight=True)
+
+    gptq.fasterquant(percdamp=percdamp, groupsize=groupsize, actorder=act_order, static_groups=static_groups)
+
+    finalW = torch.load("resultHessian.pt")
+    print(torch.equal(gptq.W, finalW))
+
+
+    """Original:
+    # Get zero and scale for the layer:
+    for name in subset:
+            gptq[name] = GPTQ(subset[name])
+            gptq[name].quantizer = Quantizer()
+            gptq[name].quantizer.configure(
+                args.wbits, perchannel=True, sym=args.sym, mse=False, trits=args.trits
+            )
+
+    # Hessian Calculation:
+    def add_batch(name):
+            def tmp(_, inp, out):
+                gptq[name].add_batch(inp[0].data, out.data)
+            return tmp
+        handles = []
+        for name in subset:
+            handles.append(subset[name].register_forward_hook(add_batch(name)))
+        for j in range(args.nsamples):
+            outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask)[0]
+        for h in handles:
+            h.remove()
+
+    # Quantizng the layers:
+    for name in subset:
+            print(i, name)
+            print('Quantizing ...')
+            gptq[name].fasterquant(
+                percdamp=args.percdamp, groupsize=args.groupsize, actorder=args.act_order, static_groups=args.static_groups
+            )
+            quantizers['model.decoder.layers.%d.%s' % (i, name)] = gptq[name].quantizer
+            gptq[name].free()
+        for j in range(args.nsamples):
+            outs[j] = layer(inps[j].unsqueeze(0), attention_mask=attention_mask)[0]
+    """
+
+# GPTQ -> Wq
+# HQQ -> Wq
---
+++ hqq.py
@@ -0,0 +1,392 @@
+import torch
+from torch import uint8, int32, Tensor
+import numpy as np
+
+
+# Bit packing logic. format: pack/unpack_nBits_target-<uint8 or int32>
+class BitPack:
+    # 8-bit
+    ################################################
+    @staticmethod
+    def pack_8bit_u8(W_q: Tensor) -> Tensor:
+        return W_q.to(uint8)
+
+    @staticmethod
+    def unpack_8bit_u8(W_q: Tensor, dtype=uint8) -> Tensor:
+        return W_q.to(dtype)
+
+    # 4-bit
+    ################################################
+    @staticmethod
+    def pack_4bit_u8(W_q: Tensor) -> Tensor:  # uint8 > uint8/2
+        W_q = W_q.to(uint8)
+        _step = int(len(W_q) / 2)
+
+        return (W_q[:_step] << 4) | W_q[_step:]
+
+    @staticmethod
+    def unpack_4bit_u8(W_q: Tensor, dtype=uint8) -> Tensor:  # uint8/2 > uint8
+        _step = W_q.shape[0]
+        tmp = torch.empty([2 * _step, W_q.shape[1]], dtype=dtype, device=W_q.device)
+
+        tmp[:_step] = (W_q & 0b11110000) >> 4
+        tmp[_step:] = W_q & 0b00001111
+
+        return tmp
+
+    # 2-bit
+    ################################################
+    @staticmethod
+    def pack_2bit_u8(W_q: Tensor) -> Tensor:  # uint8 > uint8/4
+        W_q = W_q.to(uint8)
+        _step = int(len(W_q) / 4)
+
+        return (
+            W_q[:_step] << 6
+            | W_q[_step : 2 * _step] << 4
+            | W_q[2 * _step : 3 * _step] << 2
+            | W_q[3 * _step :]
+        )
+
+    @staticmethod
+    def unpack_2bit_u8(W_q: Tensor, dtype=uint8) -> Tensor:
+        _step = W_q.shape[0]
+        tmp = torch.empty([4 * _step, W_q.shape[1]], dtype=dtype, device=W_q.device)
+
+        tmp[0 * _step : 1 * _step] = (W_q & 0b11000000) >> 6
+        tmp[1 * _step : 2 * _step] = (W_q & 0b00110000) >> 4
+        tmp[2 * _step : 3 * _step] = (W_q & 0b00001100) >> 2
+        tmp[3 * _step : 4 * _step] = W_q & 0b00000011
+
+        return tmp
+
+    # 3-bit
+    ################################################
+    @staticmethod
+    def pack_3bit_32(W_q_in: Tensor) -> Tensor:
+        W_q = torch.zeros(
+            [int(10 * np.ceil(W_q_in.shape[0] / 10.0)), W_q_in.shape[1]],
+            device=W_q_in.device,
+            dtype=int32,
+        )
+        W_q[: len(W_q_in)] = W_q_in
+        _step = int(len(W_q) / 10)
+
+        W_q = (
+            (W_q[:_step] << 27)
+            | (W_q[1 * _step : 2 * _step] << 24)
+            | (W_q[2 * _step : 3 * _step] << 21)
+            | (W_q[3 * _step : 4 * _step] << 18)
+            | (W_q[4 * _step : 5 * _step] << 15)
+            | (W_q[5 * _step : 6 * _step] << 12)
+            | (W_q[6 * _step : 7 * _step] << 9)
+            | (W_q[7 * _step : 8 * _step] << 6)
+            | (W_q[8 * _step : 9 * _step] << 3)
+            | (W_q[9 * _step : 10 * _step])
+        )
+
+        return W_q
+
+    # A bit faster than _cat version
+    @staticmethod
+    def unpack_3bit_32(W_q: Tensor, dtype=uint8) -> Tensor:
+        _step = W_q.shape[0]
+        tmp = torch.empty([10 * _step, W_q.shape[1]], dtype=dtype, device=W_q.device)
+
+        tmp[0 * _step : 1 * _step] = (W_q & 0b00111000000000000000000000000000) >> 27
+        tmp[1 * _step : 2 * _step] = (W_q & 0b00000111000000000000000000000000) >> 24
+        tmp[2 * _step : 3 * _step] = (W_q & 0b00000000111000000000000000000000) >> 21
+        tmp[3 * _step : 4 * _step] = (W_q & 0b00000000000111000000000000000000) >> 18
+        tmp[4 * _step : 5 * _step] = (W_q & 0b00000000000000111000000000000000) >> 15
+        tmp[5 * _step : 6 * _step] = (W_q & 0b00000000000000000111000000000000) >> 12
+        tmp[6 * _step : 7 * _step] = (W_q & 0b00000000000000000000111000000000) >> 9
+        tmp[7 * _step : 8 * _step] = (W_q & 0b00000000000000000000000111000000) >> 6
+        tmp[8 * _step : 9 * _step] = (W_q & 0b00000000000000000000000000111000) >> 3
+        tmp[9 * _step : 10 * _step] = W_q & 0b00000000000000000000000000000111
+
+        return tmp
+
+    # 1-bit
+    ################################################
+    @staticmethod
+    def pack_1bit_u8(W_q: Tensor) -> Tensor:
+        W_q = W_q.to(uint8)
+        _step = int(len(W_q) / 8)
+
+        return (
+            W_q[:_step] << 7
+            | W_q[1 * _step : 2 * _step] << 6
+            | W_q[2 * _step : 3 * _step] << 5
+            | W_q[3 * _step : 4 * _step] << 4
+            | W_q[4 * _step : 5 * _step] << 3
+            | W_q[5 * _step : 6 * _step] << 2
+            | W_q[6 * _step : 7 * _step] << 1
+            | W_q[7 * _step : 8 * _step]
+        )
+
+    @staticmethod
+    def unpack_1bit_u8(W_q: Tensor, dtype=uint8) -> Tensor:
+        _step = W_q.shape[0]
+        tmp = torch.empty([8 * _step, W_q.shape[1]], dtype=dtype, device=W_q.device)
+
+        tmp[0 * _step : 1 * _step] = (W_q & 0b10000000) >> 7
+        tmp[1 * _step : 2 * _step] = (W_q & 0b01000000) >> 6
+        tmp[2 * _step : 3 * _step] = (W_q & 0b00100000) >> 5
+        tmp[3 * _step : 4 * _step] = (W_q & 0b00010000) >> 4
+        tmp[4 * _step : 5 * _step] = (W_q & 0b00001000) >> 3
+        tmp[5 * _step : 6 * _step] = (W_q & 0b00000100) >> 2
+        tmp[6 * _step : 7 * _step] = (W_q & 0b00000010) >> 1
+        tmp[7 * _step : 8 * _step] = W_q & 0b00000001
+
+        return tmp
+
+
+
+unpack_view_dtype = {
+    "8bit_u8": uint8,
+    "4bit_u8": uint8,
+    "3bit_32": int32,
+    "2bit_u8": uint8,
+    "1bit_u8": uint8,
+}
+
+pack = {
+    "8bit_u8": BitPack.pack_8bit_u8,
+    "4bit_u8": BitPack.pack_4bit_u8,
+    "3bit_32": BitPack.pack_3bit_32,
+    "2bit_u8": BitPack.pack_2bit_u8,
+    "1bit_u8": BitPack.pack_1bit_u8,
+}
+
+unpack = {
+    "8bit_u8": BitPack.unpack_8bit_u8,
+    "4bit_u8": BitPack.unpack_4bit_u8,
+    "3bit_32": BitPack.unpack_3bit_32,
+    "2bit_u8": BitPack.unpack_2bit_u8,
+    "1bit_u8": BitPack.unpack_1bit_u8,
+}
+
+bit_to_packing = {
+    8: "8bit_u8",
+    4: "4bit_u8",
+    3: "3bit_32",
+    2: "2bit_u8",
+    1: "1bit_u8",
+}
+
+import torch
+import numpy as np
+from torch import uint8, int32, float16, float32, nn, Tensor
+from typing import Union
+
+
+def is_divisible(val1: int, val2: int) -> bool:
+    return int(val2 * np.ceil(val1 / val2)) == val1
+
+
+def shrink_lp_op(x: Tensor, beta: float, lp_norm: float) -> Tensor:
+    if lp_norm == 1:
+        return torch.sign(x) * torch.nn.functional.relu(torch.abs(x) - 1.0 / beta)
+    else:
+        return torch.sign(x) * torch.nn.functional.relu(
+            torch.abs(x) - (1.0 / beta) * torch.pow(torch.abs(x), lp_norm - 1)
+        )
+
+def optimize_weights(
+    tensor: Tensor,
+    scale: Tensor,
+    zero: Tensor,
+    min_max: list,
+    axis: int = 0,
+    device: Union[str, None] = None,
+    opt_params: dict = {"lp_norm": 0.7, "beta": 1e1, "kappa": 1.01, "iters": 20},
+    verbose: bool = False,
+) -> tuple:
+    lp_norm, beta, kappa, iters = (
+        opt_params["lp_norm"],
+        opt_params["beta"],
+        opt_params["kappa"],
+        opt_params["iters"],
+    )
+
+    if device is None:
+        device = tensor.device
+    else:
+        device = torch.device(device)
+
+    dtype = float16 if (device.type == "cuda") else float32
+    W_f = tensor.to(dtype=dtype, device=device)
+    scale = scale.to(dtype=dtype, device=device)
+    zero = zero.to(dtype=dtype, device=device)
+
+    best_error = 1e4
+    for i in range(iters):
+        W_q = torch.round(W_f * scale + zero).clamp(min_max[0], min_max[1])
+        W_r = (W_q - zero) / scale
+        W_e = shrink_lp_op(W_f - W_r, beta, lp_norm)
+        zero = torch.mean(W_q - (W_f - W_e) * scale, axis=axis, keepdim=True)
+        beta *= kappa
+
+        current_error = float(torch.abs(W_f - W_r).mean())
+        if verbose:
+            print(i, np.round(current_error, 6))
+        if current_error < best_error:
+            best_error = current_error
+        else:
+            break
+
+    scale = scale.to(tensor.device)
+    zero = zero.to(tensor.device)
+    del W_f, W_q, W_r, W_e
+    torch.cuda.empty_cache()
+
+    W_q = torch.round(tensor * scale + zero).clamp(min_max[0], min_max[1])
+    return W_q, scale, zero
+
+def quantize_algo(
+    tensor: Tensor,
+    nbits: int = 4,
+    channel_wise: bool = True,
+    group_size: int = 64,
+    optimize: bool = False,
+    round_zero: bool = False,
+    axis: int = 0,
+    compute_dtype: Union[torch.dtype, None] = None,
+    view_as_float: bool = False,
+    device: str = "cuda",
+) -> tuple:
+    assert nbits in [8, 4, 3, 2, 1], (
+        "nbits=" + str(nbits) + " not supported."
+    )
+    assert axis in [0, 1], "axis should be either 0 or 1"
+    if group_size is not None:
+        assert is_divisible(tensor.numel(), group_size), (
+            "group_size should be divisble by the total tensor dimensions. shape: "
+            + str(tensor.shape)
+            + ", group_size: "
+            + str(group_size)
+        )
+
+    W = tensor.float()
+    shape = W.shape
+
+    # Reshape for grouping
+    if (group_size is not None) and channel_wise:
+        W = (
+            W.reshape([-1, group_size])
+            if (axis == 1)
+            else W.reshape([group_size, -1])
+        )
+
+    # Get min/max values
+    if not channel_wise:
+        _min, _max = W.min(), W.max()
+        optimize = False
+    else:
+        _min = W.min(axis=axis, keepdim=True)[0]
+        _max = W.max(axis=axis, keepdim=True)[0]
+
+    max_v = 2**nbits - 1
+    min_v = 0
+    min_max = [min_v, max_v]
+
+    # Note: here we work with the inverse of the scale to avoid division and quantize instead via W*scale + zero, the scale is inverted later on.
+    scale = (max_v / (_max - _min)).clamp(
+        max=2e4
+    )  # clamp to avoid half-precision problems
+    zero = -_min * scale
+
+    # Round zero as in: https://github.com/casper-hansen/AutoAWQ/blob/main/awq/quantize/quantizer.py#L42C9-L42C14
+    if round_zero:
+        zero = torch.round(zero)
+
+    # Fine-tune weights
+    if optimize:
+        W_q, scale, zero = optimize_weights(
+            tensor=W,
+            scale=scale,
+            zero=zero,
+            min_max=min_max,
+            axis=axis,
+            device=device,
+        )
+    else:
+        W_q = torch.round(W * scale + zero).clamp(min_max[0], min_max[1])
+
+    # Store meta-data (we invert the scale for dequantization)
+    meta = {
+        "nbits": nbits,
+        "group_size": group_size,
+        "shape": shape,
+        "scale": 1.0 / scale,
+        "zero": zero,
+        "axis": axis,
+        "packing": bit_to_packing[nbits],
+    }
+    meta["unpack_view_dtype"] = unpack_view_dtype[meta["packing"]]
+
+    # Pack bits
+    meta["view_as_float"] = view_as_float
+
+    W_q = pack[meta["packing"]](W_q)
+
+    # cleanup
+    del W, _min, _max
+    torch.cuda.empty_cache()
+
+    return W_q, meta
+
+def quantize(
+    W: Tensor,
+    weight_quant_params: dict,
+    scale_quant_params: dict,
+    zero_quant_params: dict,
+) -> None:
+    quant_scale = scale_quant_params is not None
+    quant_zero = zero_quant_params is not None
+
+    # Quantize
+    W_q, meta = quantize_algo(
+        W,
+        device="cpu",
+        compute_dtype=torch.float16,
+        **weight_quant_params,
+    )
+    meta.update({"quant_scale": quant_scale, "quant_zero": quant_zero})
+
+    return W_q, meta
+
+
+def dequantize(W_q: Tensor, meta: dict) -> Tensor:
+    compute_dtype = meta["compute_dtype"] if ("compute_dtype" in meta) else float16
+    if meta["packing"]:
+        if meta["view_as_float"]:
+            W_q = W_q.view(meta["unpack_view_dtype"])
+        W_r = unpack[meta["packing"]](W_q, dtype=compute_dtype)
+        if (meta["group_size"] is not None) and (meta["nbits"] == 3):
+            W_r = (
+                W_r[: meta["group_size"]]
+                if (meta["axis"] == 0)
+                else W_r[:, : meta["group_size"]]
+            )
+    else:
+        W_r = W_q.to(compute_dtype)
+    W_r = ((W_r - meta["zero"]) * meta["scale"]).reshape(meta["shape"])
+    return W_r
+
+if __name__ == "__main__":
+    quant_config = {
+        'weight_quant_params': {'nbits': 4, 'channel_wise': True, 'group_size': 64, 'optimize': True, 'round_zero': True, 'axis': 0, 'view_as_float': False},
+        'scale_quant_params': None,
+        'zero_quant_params': None}
+    
+    W = torch.load("W_0.pt")
+    print(f"Original: {W}")
+
+    W_q, meta = quantize(W, **quant_config)
+    #print(f"Quantized: {W_q}")
+    #print(meta)
+
+    W_r = dequantize(W_q, meta)
+    print(f"Dequantized: {W_r}")
+    torch.save(W_r, 'HQQ_W.pt')
